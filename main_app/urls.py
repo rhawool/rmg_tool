a485d3108e3e5727e9fc2f98801c3b31d24984c0
rhawool <adsulerr@gmail.com>
2022-12-21 22:44:59 +0530

    eod 21 dec

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,15 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page, name='home_page'),
+
+
+
     path('associate_list/', views.associate_list, name='associate_list'),
-    path('bulk_import_associates/', views.bulk_import_associates, name='bulk_import_associates'),
+    path('associate_list/export_associate_data', views.export_associate_data, name='export_associate_data'),
+
+    path('add_associate_home/', views.add_associate_home, name='add_associate_home'),
+    path('add_associate_home/download_sample_format/', views.download_sample_format, name='download_sample_format'),
+    path('add_associate_home/bulk_import_associates/', views.bulk_import_associates, name='bulk_import_associates'),
+
+    path('about_page/', views.about_page, name='about_page'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.http import FileResponse, HttpResponse
 from django.shortcuts import render, redirect
 from django.utils import timezone
 from django.views.generic import DetailView, ListView
@@ -10,9 +11,13 @@ from main_app.resources import AssociateResource
 
 
 def home_page(request):
+    return render(request, "home_page.html")
+
+
+def add_associate_home(request):
     form = AddAssociateForm()
     if request.method == 'GET':
-        return render(request, "home_page.html", {'form': form})
+        return render(request, "add_associate_home.html", {'form': form})
 
     elif request.method == 'POST':
         form = AddAssociateForm(request.POST)
@@ -20,13 +25,13 @@ def home_page(request):
             try:
                 form.save()
                 messages.success(request, "Associate added successfully.")
-                return redirect("home_page")
+                return redirect("add_associate_home")
             except Exception as e:
                 messages.error(request, f"Error Adding Associate. Error code/message: {e}")
-                return redirect("home_page")
+                return redirect("add_associate_home")
     else:
         messages.error(request, "Invalid entries found. Please check and try again.")
-        return redirect("home_page")
+        return redirect("add_associate_home")
 
 
 def associate_list(request):
@@ -51,41 +56,39 @@ def bulk_import_associates(request):
             associate_resource.import_data(dataset, dry_run=False)  # Actually import now
 
             messages.success(request, "Associates added successfully")
-            return redirect("home_page")
+            return redirect("add_associate_home")
 
         else:
             messages.error(request,
                            "Error adding Associates, please input the data according to the sample format provided.")
-            return redirect("home_page")
+            return redirect("add_associate_home")
+    except Exception as e:
+        messages.error(request, f"Something went wrong, Error code/message: {e}")
+        return redirect("add_associate_home")
+
+
+def download_sample_format(request):
+    try:
+        response = FileResponse(open('media/files/sample_formats/associates_sample_format.xlsx', 'rb'))
+        return response
+
     except Exception as e:
         messages.error(request, f"Something went wrong, Error code/message: {e}")
         return redirect("home_page")
 
 
+def export_associate_data(request):
+    try:
+        associate_resource = AssociateResource()
+        dataset = associate_resource.export()
+        response = HttpResponse(dataset.xls, content_type='application/vnd.ms-excel')
+        response['Content-Disposition'] = 'attachment; filename="associate_data.xls"'
+        return response
 
-# def bulk_import_associates(request):
-#     if request.method == "POST":
-#         associate_resource = AssociateResource()
-#         dataset = Dataset()
-#         associates = Associate.objects.all()
-#         try:
-#             new_associate = request.FILES['bulk_file']
-#             if not new_associate.name.endswith('xlsx'):
-#                 messages.warning(request, 'Wrong File Format, Please Select a xlsx file type!')
-#                 return redirect("home_page")
-#             try:
-#                 dataset.load(new_associate.read())
-#                 result = associate_resource.import_data(dataset, dry_run=True)  # Test the data import
-#
-#                 if not result.has_errors():
-#                     associate_resource.import_data(dataset, dry_run=False)  # Actually import now
-#
-#                     messages.success(request, "Associates added successfully")
-#                     return redirect("home_page")
-#
-#             except Exception as e:
-#                 messages.error(request, f'Error message: {e}')
-#         except Exception as e:
-#             messages.error(request, 'Did you forget to select a file? Please Select the file...!')
-#
-#     return redirect("home_page")
+    except Exception as e:
+        messages.error(request, f"Something went wrong, Error code/message: {e}")
+        return redirect("associate_list")
+
+
+def about_page(request):
+    return render(request, "about_page.html")
